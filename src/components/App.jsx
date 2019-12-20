2caa22cccb75b44098c91d9c8cf5026c566eecac
Eisenbartl <Eisenbartl@yahoo.com>
2019-12-20 17:15:15 -0600

    basic under construction page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import RemoteReq from '../../assets/img/remotereqicon.jpg';
 
 class App extends Component {
   render() {
     return (
-      <div className="sample-container sample">
-        {"Check me out! I'm styling!"}
+      <div className='under-construction'>
+        <div className="under-construction-header">
+        <img src={ RemoteReq } alt='remotereq icon' />
+          <h1>RemoteReq</h1>
+          <h3>Hey there! Thanks for your patience while we put together our website. Visit us again soon!</h3>
+        </div>
       </div>
     );
   }
