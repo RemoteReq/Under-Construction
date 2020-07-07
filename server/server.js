7e54c3db4f0427806cd54ffd233b4585732769f5
ryanferrer <ryferr8122@gmail.com>
2020-07-07 18:51:27 -0500

    finished subscribe feature

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 // middleware
 const history = require('connect-history-api-fallback');
@@ -9,6 +10,9 @@ const PORT = 3030;
 
 // import route handlers
 const subscriber = require('./api/subscriber.js');
+
+// use CORs
+app.use(cors());
 
 // User history to intecept client requests and forward to React Router history
 app.use(history());
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,8 @@ import axios from 'axios';
 import RemoteReqIcon from '../../assets/img/remotereqicon.png';
 import RemoteReqName from '../../assets/img/remotereqname.png';
 
+const server = 'http://localhost:3030/api/subscribers';
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -10,31 +12,82 @@ class App extends Component {
     this.state = {
       email: '',
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.submitEmail = this.submitEmail.bind(this);
+    this.resetForm = this.resetForm.bind(this);
+  }
+
+  handleChange(e) {
+    e.preventDefault();
+
+    this.setState({
+      email: e.target.value,
+    }, () => { console.log(this.state.email); });
   }
 
   submitEmail(e) {
     e.preventDefault();
 
-    axios.post();
+    const { email } = this.state;
+
+    if (email) {
+      const body = {
+        email,
+      };
+
+      axios.post(server, body)
+        .then((result) => {
+          console.log('result', result);
+        });
+    } else {
+      console.log('Please enter an email');
+    }
+
+    this.resetForm();
+  }
+
+  resetForm() {
+    document.getElementById('subscribe-form').reset();
+    this.setState({
+      email: '',
+    }, () => { console.log('form reset, here is the state', this.state); });
   }
 
   render() {
     return (
       <div className='under-construction'>
-        <div className="under-construction-header">
+        <div className="under-construction-banner">
 
           <div className="under-construction-contents">
 
-            <img className='logo-img' src={ RemoteReqIcon } alt='remotereq icon' />
-            <img className='name-img' src={ RemoteReqName } alt='remotereq' />
-            <h3>Hey there! Thanks for your patience while we put together our website. Visit us again soon!</h3>
+            <div className="under-construction-header">
+              <div>
+                <img className='logo-img' src={ RemoteReqIcon } alt='remotereq icon' />
+              </div>
+              <div>
+                <img className='name-img' src={ RemoteReqName } alt='remotereq' />
+              </div>
+            </div>
 
-            <p>In the meantime, subscribe to be notified on when we go live!</p>
+            <div className="subscribe-contents">
+              <h4>Hey there! Thanks for your patience while we put together our website. Visit us again soon!</h4>
+              <p className="small-paragraph">In the meantime, subscribe to be notified on when we go live!</p>
 
-            <form>
-              <input></input>
-              <button>submit</button>
-            </form>
+              <form id="subscribe-form">
+                <input
+                  type="email"
+                  placeholder="Email"
+                  onChange={(e) => this.handleChange(e)}
+                  ></input>
+
+                <button
+                  className="button-1"
+                  type="submit"
+                  onClick={(e) => this.submitEmail(e)}
+                >Subscribe</button>
+              </form>
+            </div>
 
           </div>
         </div>
